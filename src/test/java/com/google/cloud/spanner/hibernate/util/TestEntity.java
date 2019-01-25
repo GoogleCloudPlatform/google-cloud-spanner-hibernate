1d377326c994fcc7194bba458049e93e7eae2a9d
Chengyuan Zhao <cccz@google.com>
2019-01-25 14:19:05 -0500

    pr comments

--- src/test/java/com/google/cloud/spanner/hibernate/util/TestEntity.java
+++ src/test/java/com/google/cloud/spanner/hibernate/util/TestEntity.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-package com.google.cloud.spanner.hibernate;
+package com.google.cloud.spanner.hibernate.util;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
