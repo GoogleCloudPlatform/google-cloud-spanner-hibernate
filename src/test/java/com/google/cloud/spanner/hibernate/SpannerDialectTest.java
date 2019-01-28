bfff6c2bad26f343c798674058eeda07d600d9fd
Chengyuan Zhao <cccz@google.com>
2019-01-28 11:09:18 -0500

    DDL dialect functions (#20)
    fixes #11
    overrides and tests the DDL dialect meta functions

--- src/test/java/com/google/cloud/spanner/hibernate/SpannerDialectTest.java
+++ src/test/java/com/google/cloud/spanner/hibernate/SpannerDialectTest.java
@@ -19,10 +19,14 @@
 package com.google.cloud.spanner.hibernate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Unit test for SpannerDialect.
@@ -33,6 +37,9 @@ import org.junit.Test;
 public class SpannerDialectTest {
 
   private SpannerDialect spannerDialect;
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   @Before
   public void setUp() {
@@ -47,5 +54,162 @@ public class SpannerDialectTest {
   @Test
   public void getTableExporterTest() {
     assertNotNull(this.spannerDialect.getTableExporter());
+  }
+
+  /* DDL-related function tests */
+
+  @Test
+  public void canCreateCatalogTest() {
+    assertFalse(this.spannerDialect.canCreateCatalog());
+  }
+
+  @Test
+  public void createCatalogStatementTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getCreateCatalogCommand("test");
+  }
+
+  @Test
+  public void createDropCatalogStatementTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getDropCatalogCommand("test");
+  }
+
+  @Test
+  public void canCreateSchemaTest() {
+    assertFalse(this.spannerDialect.canCreateSchema());
+  }
+
+  @Test
+  public void createSchemaStatementTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getCreateSchemaCommand("test");
+  }
+
+  @Test
+  public void dropCatalogStatementTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getDropSchemaCommand("test");
+  }
+
+  @Test
+  public void getCurrentSchemaTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getCurrentSchemaCommand();
+  }
+
+  @Test
+  public void getSchemaResolverTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getSchemaNameResolver();
+  }
+
+  @Test
+  public void hasAlterTableTest() {
+    assertTrue(this.spannerDialect.hasAlterTable());
+  }
+
+  @Test
+  public void dropConstraintsTest() {
+    assertFalse(this.spannerDialect.dropConstraints());
+  }
+
+  @Test
+  public void qualifyIndexNameTest() {
+    assertFalse(this.spannerDialect.qualifyIndexName());
+  }
+
+  @Test
+  public void getAddColumnStringTest() {
+    assertEquals("ADD COLUMN", this.spannerDialect.getAddColumnString());
+  }
+
+  @Test
+  public void getAddColumnSuffixText() {
+    assertEquals("", this.spannerDialect.getAddColumnSuffixString());
+  }
+
+  @Test
+  public void getDropKeyConstraintTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getDropForeignKeyString();
+  }
+
+  @Test
+  public void getTableTypeString() {
+    assertEquals("", this.spannerDialect.getTableTypeString());
+  }
+
+  @Test
+  public void getAddForeignKeyConstraintStringTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getAddForeignKeyConstraintString(null, null, null, null, false);
+  }
+
+  @Test
+  public void getAddForeignKeyConstraintStringSimpifiedTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getAddForeignKeyConstraintString(null, null);
+  }
+
+  @Test
+  public void getAddPrimaryKeyConstraintStringTest() {
+    this.expectedException.expect(UnsupportedOperationException.class);
+    this.spannerDialect.getAddPrimaryKeyConstraintString(null);
+  }
+
+  @Test
+  public void hasSelfReferentialForeignKeyBugTest() {
+    assertFalse(this.spannerDialect.hasSelfReferentialForeignKeyBug());
+  }
+
+  @Test
+  public void getNullColumnStringTest() {
+    assertEquals("", this.spannerDialect.getNullColumnString());
+  }
+
+  @Test
+  public void supportsCommentOnTest() {
+    assertFalse(this.spannerDialect.supportsCommentOn());
+  }
+
+  @Test
+  public void getTableCommentTEst() {
+    assertEquals("", this.spannerDialect.getTableComment("test"));
+  }
+
+  @Test
+  public void getColumnCommentTEst() {
+    assertEquals("", this.spannerDialect.getColumnComment("test"));
+  }
+
+  @Test
+  public void supportsIfExistsBeforeTableNameTest() {
+    assertFalse(this.spannerDialect.supportsIfExistsBeforeTableName());
+  }
+
+  @Test
+  public void supportsIfExistsAfterTableNameTest() {
+    assertFalse(this.spannerDialect.supportsIfExistsAfterTableName());
+  }
+
+  @Test
+  public void supportsIfExistsBeforeConstraintNameTest() {
+    assertFalse(this.spannerDialect.supportsIfExistsBeforeConstraintName());
+  }
+
+  @Test
+  public void supportsIfExistsAfterConstraintNameTest() {
+    assertFalse(this.spannerDialect.supportsIfExistsAfterConstraintName());
+  }
+
+  @Test
+  public void supportsIfExistsAfterAlterTableTest() {
+    assertFalse(this.spannerDialect.supportsIfExistsAfterAlterTable());
+  }
+
+  @Test
+  public void getDropStringTest() {
+    assertEquals("drop table test_table", this.spannerDialect.getDropTableString("test_table"));
   }
 }
