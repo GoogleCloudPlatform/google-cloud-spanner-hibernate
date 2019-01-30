78fd5db36e3be9dd48da918b37e4841c21a7a46e
dzou <dzou@users.noreply.github.com>
2019-01-30 17:55:25 -0500

    Move gradle tests to pull from upstream

--- testing/run-tests.py
+++ testing/run-tests.py
@@ -6,7 +6,7 @@ Script for running Hibernate Integration tests against the
 Spanner-Hibernate dialect.
 '''
 
-HIBERNATE_TESTS_REPO = 'https://github.com/dzou/hibernate-orm.git'
+HIBERNATE_TESTS_REPO = 'https://github.com/hibernate/hibernate-orm.git'
 
 # Install
 subprocess.run('mvn install -DskipTests -f ../pom.xml', shell=True)
@@ -19,13 +19,15 @@ else:
   subprocess.run(['git', 'clone', HIBERNATE_TESTS_REPO])
 
 # Copy the JDBC driver to directory if it does not already exist.
-if 'CloudSpannerJDBC42.jar' not in os.listdir('hibernate-orm/libs'):
+if not os.path.isdir('hibernate-orm/libs') or \
+    'CloudSpannerJDBC42.jar' not in os.listdir('hibernate-orm/libs'):
   print('Downloading Spanner JDBC driver')
-  subprocess.run('gsutil cp gs://spanner-jdbc-bucket/CloudSpannerJDBC42.jar hibernate-orm/libs/', shell=True)
+  subprocess.run('gsutil cp gs://spanner-jdbc-bucket/CloudSpannerJDBC42.jar hibernate-orm/libs/CloudSpannerJDBC42.jar', shell=True)
 
-# Update the databases.gradle file with the current version.
+# Patch the hibernate-orm repo with custom Gradle files for testing
 subprocess.run('cp databases.gradle hibernate-orm/gradle/databases.gradle', shell=True)
+subprocess.run('cp documentation.gradle hibernate-orm/documentation/documentation.gradle', shell=True)
 
 # Run some tests.
 # Modify this to filter down to a specific test with --test TEST_NAME
-subprocess.run('hibernate-orm/gradlew test -p hibernate-orm/documentation', shell=True)
+subprocess.run('hibernate-orm/gradlew test -p hibernate-orm/documentation -Pdb=spanner --tests TuplizerTest', shell=True)
