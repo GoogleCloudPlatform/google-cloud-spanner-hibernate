c9ad30f58253a9564c501c45e0993918b2c9c60d
dzou <dzou@users.noreply.github.com>
2020-09-10 14:22:27 -0400

    checkstyle

--- google-cloud-spanner-hibernate-dialect/src/test/java/com/google/cloud/spanner/hibernate/entities/Account.java
+++ google-cloud-spanner-hibernate-dialect/src/test/java/com/google/cloud/spanner/hibernate/entities/Account.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 package com.google.cloud.spanner.hibernate.entities;
 
 import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * Sample entity with a {@link BigDecimal} column to demonstrate NUMERIC support.
+ */
 @Entity
 public class Account {
 
@@ -17,6 +38,9 @@ public class Account {
   // Default constructor for Hibernate
   Account() {}
 
+  /**
+   * Constructs the Account entity.
+   */
   public Account(long id, String name, BigDecimal amount) {
     this.id = id;
     this.name = name;
