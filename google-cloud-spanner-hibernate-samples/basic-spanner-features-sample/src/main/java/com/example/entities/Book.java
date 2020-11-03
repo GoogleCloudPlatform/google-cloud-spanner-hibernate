531ca35bd215c04dfc7427d0aea382781b2ba3bb
dzou <dzou@users.noreply.github.com>
2020-11-03 15:34:39 -0500

    checkstyle

--- google-cloud-spanner-hibernate-samples/basic-spanner-features-sample/src/main/java/com/example/entities/Book.java
+++ google-cloud-spanner-hibernate-samples/basic-spanner-features-sample/src/main/java/com/example/entities/Book.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019-2020 Google LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 package com.example.entities;
 
 import java.util.UUID;
@@ -22,7 +40,9 @@ public class Book {
 
   private String author;
 
-  public Book() {}
+  public Book() {
+
+  }
 
   public Book(String title, String author) {
     this.title = title;
