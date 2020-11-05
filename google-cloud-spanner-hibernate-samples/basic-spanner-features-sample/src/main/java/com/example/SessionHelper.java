291a30b26ba9119d7cbab11dc3f15ab7e9b68bdd
dzou <dzou@users.noreply.github.com>
2020-11-05 17:20:53 -0500

    Checkstyle

--- google-cloud-spanner-hibernate-samples/basic-spanner-features-sample/src/main/java/com/example/SessionHelper.java
+++ google-cloud-spanner-hibernate-samples/basic-spanner-features-sample/src/main/java/com/example/SessionHelper.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019-2020 Google LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 package com.example;
 
 import org.hibernate.Session;
@@ -20,6 +38,9 @@ public class SessionHelper {
     this.sessionFactory = sessionFactory;
   }
 
+  /**
+   * Creates a read-write session.
+   */
   public Session createReadWriteSession() {
     Session session = sessionFactory.openSession();
     session.doWork(conn -> {
@@ -29,6 +50,9 @@ public class SessionHelper {
     return session;
   }
 
+  /**
+   * Creates a read-only session.
+   */
   public Session createReadOnlySession() {
     Session session = sessionFactory.openSession();
     session.doWork(conn -> {
@@ -38,6 +62,9 @@ public class SessionHelper {
     return session;
   }
 
+  /**
+   * Create a session for exact stale reads at {@code stalenessSeconds} in the past.
+   */
   public Session createExactStaleReadSession(int stalenessSeconds) {
     Session session = sessionFactory.openSession();
     session.doWork(conn -> {
