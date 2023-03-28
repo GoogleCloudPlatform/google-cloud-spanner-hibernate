2d1997d56eb7152b94eba720cb2e632407493c1d
Arpan Mishra <arpanmishra@google.com>
2023-03-28 18:50:14 +0530

    minor fixes.

--- google-cloud-spanner-hibernate-testing/src/test/java/org/hibernate/dialect/ForeignKeyTest.java
+++ google-cloud-spanner-hibernate-testing/src/test/java/org/hibernate/dialect/ForeignKeyTest.java
@@ -3,7 +3,7 @@ package org.hibernate.dialect;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.testing.transaction.TransactionUtil.doInHibernate;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -23,13 +23,6 @@ import org.hibernate.jpa.test.BaseCoreFunctionalTestCase;
 import org.junit.Test;
 
 public class ForeignKeyTest extends BaseCoreFunctionalTestCase {
-
-  private static final String SESSION_CART_FOREIGN_KEY_CONSTRAINT_NAME = "Fk_sessionDetails_cartId";
-  private static final String ITEM_CART_FOREIGN_KEY_CONSTRAINT_NAME = "Fk_itemDetails_cartId";
-  private static final String DELETE_RULE_COLUMN_NAME = "DELETE_RULE";
-  private static final String CONSTRAINT_NAME_COLUMN_NAME = "CONSTRAINT_NAME";
-  private static final String DELETE_RULE_CASCADE_VALUE = "CASCADE";
-  private static final String DELETE_RULE_DEFAULT_VALUE = "NO ACTION";
 
   @Override
   protected Class<?>[] getAnnotatedClasses() {
@@ -64,7 +57,7 @@ public class ForeignKeyTest extends BaseCoreFunctionalTestCase {
         session.persist(cartSession);
       });
 
-    verifyOnDeleteCascadeDDL();
+    verifyOnDeleteCascadeReferentialConstraints();
   }
 
   @Test
@@ -102,30 +95,28 @@ public class ForeignKeyTest extends BaseCoreFunctionalTestCase {
 
   @Test
   public void testForeignKeyConstraint_verifyInvalidInserts() {
-    try {
-      doInHibernate(this::sessionFactory, session -> {
-        Cart cart = new Cart();
-        cart.setCartId(3L);
-        cart.setItems(new HashSet<>());
 
-        Item item = new Item();
-        item.setItemId(3L);
-        item.setCart(cart);
+    final PersistenceException ex = assertThrows(
+        PersistenceException.class, () -> doInHibernate(this::sessionFactory, session -> {
+          Cart cart = new Cart();
+          cart.setCartId(3L);
+          cart.setItems(new HashSet<>());
 
-        CartSession cartSession = new CartSession();
-        cartSession.setSessionId(3L);
-        cartSession.setCart(cart);
+          Item item = new Item();
+          item.setItemId(3L);
+          item.setCart(cart);
 
-        cart.addItem(item);
-        cart.setSession(cartSession);
+          CartSession cartSession = new CartSession();
+          cartSession.setSessionId(3L);
+          cartSession.setCart(cart);
 
-        session.persist(item);
-      });
-      fail("Expected exception");
-    } catch (final PersistenceException ex) {
-      assertThat(ex.getCause().getCause().getMessage()).contains("Foreign key constraint `Fk_itemDetails_cartId` is "
-          + "violated on table `Item`");
-    }
+          cart.addItem(item);
+          cart.setSession(cartSession);
+
+          session.persist(item);
+        }));
+    assertThat(ex.getCause().getCause().getMessage()).contains("Foreign key constraint `Fk_itemDetails_cartId` is "
+        + "violated on table `Item`");
   }
 
   @Test
@@ -163,14 +154,14 @@ public class ForeignKeyTest extends BaseCoreFunctionalTestCase {
     });
 
 
-    verifyOnDeleteCascadeDDL();
+    verifyOnDeleteCascadeReferentialConstraints();
   }
 
   /**
    * This method uses a JDBC connection to query for the tables that exist in the Spanner database
    * and verifies the foreign relationships are created between tables.
    */
-  private static void verifyOnDeleteCascadeDDL() throws SQLException {
+  private static void verifyOnDeleteCascadeReferentialConstraints() throws SQLException {
     Configuration configuration = new Configuration();
 
     try (Connection jdbcConnection =
@@ -182,10 +173,10 @@ public class ForeignKeyTest extends BaseCoreFunctionalTestCase {
       final Map<String, String> actualReferentialQueryDetails = new HashMap<>();
       final ResultSet tablesResultSet = statement.getResultSet();
       while (tablesResultSet.next()) {
-        actualReferentialQueryDetails.put(tablesResultSet.getString(CONSTRAINT_NAME_COLUMN_NAME), tablesResultSet.getString(DELETE_RULE_COLUMN_NAME));
+        actualReferentialQueryDetails.put(tablesResultSet.getString("CONSTRAINT_NAME"), tablesResultSet.getString("DELETE_RULE"));
       }
-      assertThat(actualReferentialQueryDetails).contains(new AbstractMap.SimpleEntry(ITEM_CART_FOREIGN_KEY_CONSTRAINT_NAME, DELETE_RULE_CASCADE_VALUE));
-      assertThat(actualReferentialQueryDetails).contains(new AbstractMap.SimpleEntry(SESSION_CART_FOREIGN_KEY_CONSTRAINT_NAME, DELETE_RULE_DEFAULT_VALUE));
+      assertThat(actualReferentialQueryDetails).contains(new AbstractMap.SimpleEntry("Fk_itemDetails_cartId", "CASCADE"));
+      assertThat(actualReferentialQueryDetails).contains(new AbstractMap.SimpleEntry("Fk_sessionDetails_cartId", "NO ACTION"));
     }
 
   }
