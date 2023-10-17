7599b917d96fea2f522f750e9c41761476c7369a
Knut Olav Løite <koloite@gmail.com>
2023-10-17 14:04:53 +0200

    docs: add full JPA sample

---
+++ google-cloud-spanner-hibernate-samples/spring-data-jpa-full-sample/src/main/java/com/google/cloud/spanner/sample/SampleApplication.java
@@ -0,0 +1,9 @@
+package com.google.cloud.spanner.sample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication extends CommandLineRunner {
+
+}
