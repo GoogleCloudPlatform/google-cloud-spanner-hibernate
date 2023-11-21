4b9f286ec2f8998054865db7409ed4fb80f69603
Knut Olav Løite <koloite@gmail.com>
2023-11-21 21:58:03 +0100

    build: try with maven opts

--- .kokoro/stage.sh
+++ .kokoro/stage.sh
@@ -45,6 +45,7 @@ mvn clean deploy -B \
   -Dgpg.homedir=${GPG_HOMEDIR} \
   -P release
 
+export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
 # promote release
 # if [[ -n "${AUTORELEASE_PR}" ]]
 # then
