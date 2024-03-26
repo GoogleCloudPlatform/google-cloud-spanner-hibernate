cf9769e59b7669260fbe00bdb28f5f52d4cffad1
Knut Olav Løite <koloite@gmail.com>
2024-03-26 14:50:39 +0100

    feat: transaction tagging
    Adds support for transaction tags using an interceptor that
    traverses the call stack up to the method that actually
    started the transaction. The tag is then determined by:
    1. Looking for an @TransactionTag annotation on the method.
    2. Or, if enabled, automatically generating a tag from the
       class name and method name.
    
    The option to automatically add tags based on class and method
    name can be enabled through a system property. This makes it
    possible to enable this feature only during debugging, and
    disable it again when it is no longer needed.

---
+++ google-cloud-spanner-hibernate-samples/spring-data-jpa-full-sample/src/main/java/com/google/cloud/spanner/sample/TaggingHibernatePropertiesCustomizer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019-2023 Google LLC
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package com.google.cloud.spanner.sample;
+
+import com.google.cloud.spanner.hibernate.TransactionTagInterceptor;
+import com.google.common.collect.ImmutableSet;
+import java.util.Map;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.stereotype.Component;
+
+/** This component adds the {@link TransactionTagInterceptor} to the sample application. */
+@Component
+public class TaggingHibernatePropertiesCustomizer implements HibernatePropertiesCustomizer {
+  @Override
+  public void customize(Map<String, Object> hibernateProperties) {
+    hibernateProperties.put(AvailableSettings.INTERCEPTOR, new TransactionTagInterceptor(
+        ImmutableSet.of(SampleApplication.class.getPackageName()), false));
+  }
+}
