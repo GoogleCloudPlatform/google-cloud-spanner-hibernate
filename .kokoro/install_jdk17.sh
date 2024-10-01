5ec71cfb6d6f30f6c5326df5cebddf3d24ac0114
Knut Olav Løite <koloite@gmail.com>
2024-10-01 11:10:51 +0200

    build: update Maven download location

--- .kokoro/install_jdk17.sh
+++ .kokoro/install_jdk17.sh
@@ -19,9 +19,9 @@ set -eo pipefail
 
 sudo apt install -y openjdk-17-jdk openjdk-17-jre
 
-wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-tar -xvf apache-maven-3.6.3-bin.tar.gz
-mv apache-maven-3.6.3 /opt/
+wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -xvf apache-maven-3.9.9-bin.tar.gz
+mv apache-maven-3.9.9 /opt/
 
 export M2_HOME='/opt/apache-maven-3.6.3'
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
